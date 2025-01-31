bd0937e03c5349c4f2aedf1d07d2daca7cc6465b
Lunev Dmitriy <lunev.da@phystech.edu>
2025-01-31 23:11:13 +0300

    Edit .gitignore not to add some shit like figures, outputs and e.t.c. Optimize force calculation. Now good parameters choice is necessary

--- src/main.cpp
+++ src/main.cpp
@@ -12,47 +12,41 @@
 
 #include "Vector.hpp"
 
-const float deltatime = 1e-6;
 // Половина размера ячейки, в которой происходит симуляция
 // [-length, length]x[-length, length]x[-length, length]
-const float length = 1.e-3;
+const float length = 1.e-2;
 const float bolzman_constant = 1.38 * 1.e-23;
 
+// Параметры для аргона
+const float epsilon = 120 * bolzman_constant;
+const float sigma = 0.34 * 1.e-9;
+
+const float mass = 18 * 1.66 * 1.e-27;
+
+const float deltatime = 1e-5;
+
 Vector force(const Vector& source, const Vector& target) {
-    static const float epsilon = 120 * bolzman_constant;
-    static const float sigma = 0.34 * 1.e-9;
+    
+    Vector sum_force = zero;
 
     // На частицу может действовать не только сама частица, но и ее "образ" при периодическом отображении
-    // Поэтому надо учитывать всех ближайших соседей. Большая часть из них будет отсечена по расстоянию (см. ниже)
-    std::vector<Vector> possible_source = {
-        source, source + Vector(length, 0, 0), source + Vector(length, 0, 0), 
-        source + Vector(0, length, 0), source + Vector(0, -length, 0), 
-        source + Vector(0, 0, length), source + Vector(0, 0, -length), 
-        source + Vector(length, length, 0), source + Vector(length, -length, 0), 
-        source + Vector(-length, length, 0), source + Vector(-length, -length, 0), 
-        source + Vector(length, 0, length), source + Vector(length, 0, -length),
-        source + Vector(-length, 0, length), source + Vector(-length, 0, -length),
-        source + Vector(0, length, length), source + Vector(0, length, -length),
-        source + Vector(0, -length, length), source + Vector(0, -length, -length),
-        source + Vector(length, length, length), source + Vector(length, length, -length),
-        source + Vector(length, -length, length), source + Vector(length, -length, -length),
-        source + Vector(-length, length, length), source + Vector(-length, length, -length),
-        source + Vector(-length, -length, length), source + Vector(-length, -length, -length),
-    };
+    // Поэтому надо учитывать всех ближайших соседей. Большая часть из них будет отсечена по расстоянию 
+    for (int i = 0; i < 27; i++) {
+        int digits[3] = {i / 9, i / 3 % 3, i % 3};
+        for (int j = 0; j < 3; j++) {
+            if (digits[j] == 2) digits[j] = -1;
+        }
+        
+        Vector possible_source = source + length * Vector(digits[0], digits[1], digits[2]);
 
-    std::transform(possible_source.cbegin(), possible_source.cend(), possible_source.begin(), [target](const Vector vec){return vec - target;});
+        float distance = (possible_source - target).magnitude();
 
-    // Обрубаем потенциал на значении r = 2.5 sigma
-    std::remove_if(possible_source.begin(), possible_source.end(), [](const Vector& vec){return vec.magnitude() > 2.5 * sigma;});
+        if (distance > 2.5 * sigma) continue;
 
-    std::vector<float> distances(possible_source.size());
-    std::transform(possible_source.cbegin(), possible_source.cend(), distances.begin(), [](const Vector& vec){return vec.magnitude();});
-
-    for (size_t i = 0; i < possible_source.size(); i++) {
-        possible_source[i] = (48 * epsilon / sigma / sigma) * (powf(sigma / distances[i], 14) - powf(sigma / distances[i], 8) / 2) * possible_source[i];
+        sum_force += (48 * epsilon / sigma / sigma) * (powf(sigma / distance, 14) - powf(sigma / distance, 8) / 2) * (target - possible_source);
     }
 
-    return std::accumulate(possible_source.cbegin(), possible_source.cend(), zero, [](const Vector& left, const Vector& right){return left + right;});
+    return sum_force;
 }
 
 void clamp_to_box(Vector& vec) {
@@ -80,8 +74,6 @@ void clamp_to_box(Vector& vec) {
 }
 
 int main() {
-    const float mass = 18 * 1.66 * 1.e-27;
-
     const int number_of_particles = 1000;
 
     Vector positions[number_of_particles];
@@ -96,35 +88,45 @@ int main() {
 
     std::default_random_engine reng(seed);
     std::uniform_real_distribution<float> dist(-length, length); // распределение положения частиц
+
     float max_possible_velocity = sqrtf(2 * bolzman_constant * 300 / mass);
-    std::uniform_real_distribution<float> vel_dist(-1, 1); // распределение скоростей частиц
-    std::normal_distribution norm_dist(max_possible_velocity, max_possible_velocity / 2); // распределение модуля вектора
+    std::uniform_real_distribution<float> vel_dist(-max_possible_velocity, max_possible_velocity); // распределение скоростей частиц
 
     std::cout << "Initialize points" << std::endl;
 
     // Случайная инициализация точек и скоростей
     for (int i = 0; i < number_of_particles; i++) {
         float x_init = dist(reng), y_init = dist(reng), z_init = dist(reng);
-        float phi_init = vel_dist(reng), theta_init = vel_dist(reng);
+        float vx_init = vel_dist(reng), vy_init = vel_dist(reng), vz_init = vel_dist(reng);
 
         positions[i].x = x_init;
         positions[i].y = y_init;
         positions[i].z = z_init;
 
-        velocities[i].x = cos(phi_init) * cos(theta_init);
-        velocities[i].y = sin(phi_init) * cos(theta_init);
-        velocities[i].z = sin(theta_init);
-
-        velocities[i] = norm_dist(reng) * velocities[i];
+        velocities[i].x = vx_init;
+        velocities[i].y = vy_init;
+        velocities[i].z = vz_init;
     }
 
     std::cout << "Done" << std::endl;
+    
+    // Записываем в файл
+    std::cout << "Write a file..."; 
+    std::filesystem::create_directory("output");
+    std::ofstream file(std::string("output/step") + std::to_string(0) + ".csv");
+    if (!file.is_open()) return -1;
+    for (int i = 0; i < number_of_particles; i++) {
+        file << positions[i].x << " " << positions[i].y << " " << positions[i].z << " "
+            << velocities[i].x << " " << velocities[i].y << " " << velocities[i].z << std::endl;
+    }
+    std::cout << "Done." << std::endl;
 
-    for (int step = 0; step < 10; step++) {
 
-        std::cout << "Step " << step << ":" << std::endl;
+    for (int step = 1; step <= 10; step++) {
 
-        std::cout << "Calculate forces..." << std::endl;
+        std::cout << std::endl << "Step " << step << ":" << std::endl;
+
+        std::cout << "Calculate forces...";
 
         // считаем силы, действующие на частицы
         for (int i = 0; i < number_of_particles; i++) {
@@ -136,18 +138,18 @@ int main() {
             }
         }
 
-        std::cout << std::endl << "Done." << std::endl;
+        std::cout << "Done." << std::endl;
 
         // алгоритм Верле
 
         // считаем новые позиции
 
-        std::cout << "Calculate positions..." << std::endl;
+        std::cout << "Calculate positions...";
         for (int i = 0; i < number_of_particles; i++) {
             next_positions[i] = positions[i] + deltatime * velocities[i] + deltatime * deltatime / 2 / mass * forces[i];
             clamp_to_box(next_positions[i]);
         }
-        std::cout << std::endl << "Done." << std::endl;
+        std::cout << "Done." << std::endl;
 
         std::cout << "Calculate new forces...";
         // снова считаем силы
@@ -160,20 +162,21 @@ int main() {
                 new_forces[i] += force(next_positions[i], next_positions[j]);
             }
         }
-        std::cout << std::endl << "Done." << std::endl;
+        std::cout << "Done." << std::endl;
 
         // считаем скорости
         std::cout << "Calculate velocities...";
         for (int i = 0; i < number_of_particles; i++) {
-            velocities[i] = velocities[i] + deltatime / 2 * (forces[i] + new_forces[i]);
+            next_velocities[i] = velocities[i] + deltatime / 2 * (forces[i] + new_forces[i]);
         }
         std::cout << "Done." << std::endl;
 
-        std::cout << "Swap positions";
+        std::cout << "Swap positions and velocities...";
         for (int i = 0; i < number_of_particles; i++) {
             std::swap(next_positions[i], positions[i]);
+            std::swap(next_velocities[i], velocities[i]);
         }
-        std::cout << std::endl << "Done." << std::endl;
+        std::cout << "Done." << std::endl;
 
         // Записываем в файл
         std::cout << "Write a file..."; 
@@ -184,7 +187,7 @@ int main() {
             file << positions[i].x << " " << positions[i].y << " " << positions[i].z << " "
                 << velocities[i].x << " " << velocities[i].y << " " << velocities[i].z << std::endl;
         }
-        std::cout << std::endl << "Done." << std::endl;
+        std::cout << "Done." << std::endl;
     }
 
     return 0;
